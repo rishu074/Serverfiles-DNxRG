b42900585a672c27d02f4d5587aad3fd8644062b
NotRoyadma <royadma2314@gmail.com>
2023-01-21 16:38:29 +0530

    Finally security is tough

--- middlewares/ip_checkers.ts
+++ middlewares/ip_checkers.ts
@@ -7,7 +7,11 @@ export default function IpCheckersMiddleware(req: Request, res: Response, next: 
     if(!REQUEST_IP) {
         res.status(500).send("Server side error, Please contact any admin")
     }
-
+    
+    // check for ip blacklist
+    if(process.WhitelistedIPS.indexOf(REQUEST_IP?.toString() as string) === -1) {
+        return res.status(403).end("403 Forbidden")
+    }
     // log to requests
     try {
         CreateRequestLog(REQUEST_IP as string, req.originalUrl ,req.headers['user-agent'] as string, Date.now())
@@ -16,10 +20,6 @@ export default function IpCheckersMiddleware(req: Request, res: Response, next: 
         return res.status(500).end("Server side error, Please contact any admin")
     }
 
-    // check for ip blacklist
-    if(process.WhitelistedIPS.indexOf(REQUEST_IP?.toString() as string) === -1) {
-        return res.status(403).end("403 Forbidden")
-    }
 
     next()
 }
