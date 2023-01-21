30cd9797e8b61597a3daba2f7aedcfe8bd1d43fc
NotRoyadma <royadma2314@gmail.com>
2023-01-21 17:07:28 +0530

    Added public directory with ip protection

--- routes/register.ts
+++ routes/register.ts
@@ -1,13 +1,15 @@
-import { Express } from "express";
+import { Express, static as st } from "express";
 import IpCheckersMiddleware from "../middlewares/ip_checkers";
 import InfoLog from "../loggers/info";
 import StartWorkers from "../workers/HandleWorkers";
 import GetFile from "./files/get_file";
 import RenderIndex from "./renders";
+import path from 'path'
 
 export default async function RegisterRoutes(app: Express) {
     InfoLog("Registering routes and middlewares")
     app.use(IpCheckersMiddleware)
+    app.use(st(path.join(process.cwd(), "public")))
 
     app.get("/", RenderIndex)
     app.get("/file/*", GetFile)
